955b230950b07df68c713482801e81483a27d44d
carlos <carlosdelacruz@ucm.es>
2018-03-15 19:56:03 +0100

    commit
    He hecho la primera prueba, si dais al html PruebaCarlos os inicia en una pagina para que pulseis un boton, y si lo haceis os lleva a la del producto en cuestion

--- src/datos/DatosProductos.java
+++ src/datos/DatosProductos.java
@@ -9,16 +9,58 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet ("/Producto")
+@WebServlet(urlPatterns = {"/DatosProductos"})
 public class DatosProductos extends HttpServlet {
 		  private static final long serialVersionUID = 1L;
 		 
-	 
-		  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		    response.getWriter().append("Servlet de Prueba ");
-		  }
-		 
-		  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		    doGet(request, response);
-		  }
+		   
+		    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+		            throws ServletException, IOException {
+		        //Vienes de la pagina 1 y vas a la 2
+		        RequestDispatcher view = request.getRequestDispatcher("pruebaCarlos.html");
+		        view.forward(request, response);
+		    }
+
+		// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+		    /**
+		     * Handles the HTTP <code>GET</code> method.
+		     *
+		     * @param request servlet request
+		     * @param response servlet response
+		     * @throws ServletException if a servlet-specific error occurs
+		     * @throws IOException if an I/O error occurs
+		     */
+		    @Override
+		    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		            throws ServletException, IOException {
+		        processRequest(request, response);
+		    }
+
+		    /**
+		     * Handles the HTTP <code>POST</code> method.
+		     *
+		     * @param request servlet request
+		     * @param response servlet response
+		     * @throws ServletException if a servlet-specific error occurs
+		     * @throws IOException if an I/O error occurs
+		     */
+		    @Override
+		    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		            throws ServletException, IOException {
+		        processRequest(request, response);
+		    }
+
+		    /**
+		     * Returns a short description of the servlet.
+		     *
+		     * @return a String containing servlet description
+		     */
+		    @Override
+		    public String getServletInfo() {
+		        return "Main-Producto";
+		    }// </editor-fold>
+
 		}
+
+		  
+		  
