6e06439fcdc00610d0740b0b0b0d656972355c93
DaniDauron <37369310+DaniDauron@users.noreply.github.com>
2018-03-16 10:04:53 +0100

    Commit
    Testeo para navegacion a juego

--- src/datos/PruebaCarlos.java
+++ src/datos/PruebaCarlos.java
@@ -12,64 +12,83 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class PruebaCarlos
  */
-@WebServlet(urlPatterns = {"/PruebaCarlos"})
+@WebServlet(urlPatterns = { "/PruebaCarlos" })
 public class PruebaCarlos extends HttpServlet {
 
-   /**
-    * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-    * methods.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-           throws ServletException, IOException {
-       //Si vienes de la pagina 2, vas a la 3
-       RequestDispatcher view = request.getRequestDispatcher("ficha.jsp");
-       view.forward(request, response);
-   }
+	/**
+	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+	 * methods.
+	 *
+	 * @param request
+	 *            servlet request
+	 * @param response
+	 *            servlet response
+	 * @throws ServletException
+	 *             if a servlet-specific error occurs
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Si vienes de la pagina 2, vas a la 3
 
-//<editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-   /**
-    * Handles the HTTP <code>GET</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-   @Override
-   protected void doGet(HttpServletRequest request, HttpServletResponse response)
-           throws ServletException, IOException {
-	   String numero = request.getParameter("juego");   
-			   processRequest(request, response);
-   }
+		String numero = request.getParameter("juego");
 
-   /**
-    * Handles the HTTP <code>POST</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-   @Override
-   protected void doPost(HttpServletRequest request, HttpServletResponse response)
-           throws ServletException, IOException {
-       processRequest(request, response);
-   }
+		if (Integer.parseInt(numero) == 1) {
+			RequestDispatcher view = request.getRequestDispatcher("http://www.google.es");
+			view.forward(request, response);
+		}else{
+		RequestDispatcher view = request.getRequestDispatcher("ficha.jsp");
+		view.forward(request, response);
+		}
+	}
 
-   /**
-    * Returns a short description of the servlet.
-    *
-    * @return a String containing servlet description
-    */
-   @Override
-   public String getServletInfo() {
-       return "Producto-main";
-   }// </editor-fold>
+	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on
+	// the + sign on the left to edit the code.">
+	/**
+	 * Handles the HTTP <code>GET</code> method.
+	 *
+	 * @param request
+	 *            servlet request
+	 * @param response
+	 *            servlet response
+	 * @throws ServletException
+	 *             if a servlet-specific error occurs
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * Handles the HTTP <code>POST</code> method.
+	 *
+	 * @param request
+	 *            servlet request
+	 * @param response
+	 *            servlet response
+	 * @throws ServletException
+	 *             if a servlet-specific error occurs
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * Returns a short description of the servlet.
+	 *
+	 * @return a String containing servlet description
+	 */
+	@Override
+	public String getServletInfo() {
+		return "Producto-main";
+	}// </editor-fold>
 
 }
-
