d806d0c7c377fed9f7bef22ae0ae11aee6cef7d3
OlgaMaestre <olgamm16@gmail.com>
2018-03-19 14:38:46 +0100

    commit
    commit

--- src/datos/ListadoDao.java
+++ src/datos/ListadoDao.java
@@ -4,20 +4,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListadoDao {
 
-	public static Producto recuperarJuego(String numero) {
+	public static List recuperarElementoListado (String numero) {
 
 		// TODO Auto-generated method stub
 
 		Connection con = null;
 		Statement st = null;
 		ResultSet rs = null;
-
+	
+		
+		List slider= new ArrayList(6);
 		
 		
-		Producto x;
+		ElementoListado x;
 
 		try {
 			String driverClassName = "com.mysql.jdbc.Driver";
@@ -36,21 +40,34 @@ public class ListadoDao {
 			String query = "SELECT id,nombre,precio from producto ";
 			rs = st.executeQuery(query);
 			System.out.println("bbbbb");
-			x = new Producto();
 			
-			while (rs.next()) {
+			x = new ElementoListado();
+			
+		
+			for (int i=0; i<6 && rs.next(); i++){
+				x.setId(rs.getInt("id"));
+				x.setNombreProducto(rs.getString("nombre"));
+				x.setPrecio(rs.getFloat("precio"));
+				slider.add(x);
+			}
+			
+			
+			/*
+			 * while (rs.next() && continuar) {
 				System.out.println("nombre es :" + rs.getString("nombre"));
-				x.setIdProd(rs.getInt("id"));
+				x.setId(rs.getInt("id"));
 				x.setNombreProducto(rs.getString("nombre"));
 				x.setPrecio(rs.getFloat("precio"));
 				
 			}
+			*/
+			
 		} catch (Exception a) {
 			System.out.println("error es " + a.getMessage());
-			x= new Producto();
+			x= new ElementoListado();
 		}
 
-		return x;
+		return slider;
 
 	}
 	
