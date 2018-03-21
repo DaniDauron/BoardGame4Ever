8640fe1e58e4c5bd3b58cce6bf806256a495a648
DaniDauron <37369310+DaniDauron@users.noreply.github.com>
2018-03-21 18:08:01 +0100

    commit
    navegacion y busqueda

--- src/servicio/VerListadoProductos.java
+++ src/servicio/VerListadoProductos.java
@@ -36,40 +36,55 @@ public class VerListadoProductos extends HttpServlet {
 		
 		if(pagina.equals("todo")){
 			 juegos = ListadoDao.recuperarElementoListado();
+			 request.setAttribute("tituloPagina", "Todos nuestros productos");
 		}else if(pagina.equals("novedades")){
-			juegos = ListadoDao.recuperarListaNovedades();			
+			juegos = ListadoDao.recuperarListaNovedades();		
+			request.setAttribute("tituloPagina", "Mira que novedades");
 		}else if(pagina.equals("tablero")){
 			juegos = ListadoDao.recuperarListaPorTipoTablero();
+			request.setAttribute("tituloPagina", "Los juegos de tablero que te enamorarán");
 		}else if(pagina.equals("dados")){
 			juegos = ListadoDao.recuperarListaPorTipoDados();
+			request.setAttribute("tituloPagina", "Lanzo dado y me llevo el botín");
 		}else if(pagina.equals("cartas")){
 			juegos = ListadoDao.recuperarListaPorTipoCartas();
+			request.setAttribute("tituloPagina", "Confia en el corazón de las cartas");
 		}else if(pagina.equals("parejas")){
 			juegos = ListadoDao.recuperarListaPorNumeroJugadores("parejas");
+			request.setAttribute("tituloPagina", "Lo mejor para las parejas");
 		}else if(pagina.equals("trios")){
 			juegos = ListadoDao.recuperarListaPorNumeroJugadores("trios");
+			request.setAttribute("tituloPagina", "El triángulo de la discordia");
 		}else if(pagina.equals("multitud")){
 			juegos = ListadoDao.recuperarListaPorNumeroJugadores("multitud");
+			request.setAttribute("tituloPagina", "Solo 10 minutos para tu proximo turno");
 		}else if(pagina.equals("ninios")){
 			juegos = ListadoDao.recuperarListaPorEdades("ninios");
+			request.setAttribute("tituloPagina", "Enseña a los mas pequeños el placer de los Meeples");
 		}else if(pagina.equals("adolescentes")){
 			juegos = ListadoDao.recuperarListaPorEdades("adolescentes");
+			request.setAttribute("tituloPagina", "Juegos para picar la curiosidad de los jovenes");
 		}else if(pagina.equals("adultos")){
 			juegos = ListadoDao.recuperarListaPorEdades("adultos");
+			request.setAttribute("tituloPagina", "Desata toda tu ira sobre tus compañeros, son mayorcitos, lo entenderán");
 		}else if(pagina.equals("ligera")){
 			juegos = ListadoDao.recuperarListaPorDuracion("ligera");
+			request.setAttribute("tituloPagina", "Para jugar entre birras");
 		}else if(pagina.equals("media")){
 			juegos = ListadoDao.recuperarListaPorDuracion("media");
+			request.setAttribute("tituloPagina", "Pasando un buen rato entre piezas");
 		}else if(pagina.equals("larga")){
 			juegos = ListadoDao.recuperarListaPorDuracion("larga");
-		}else if(pagina.equals("buscar")){			
+			request.setAttribute("tituloPagina", "Prepara el saco de dormir, y que nadie toque el tablero por la noche");
+		}else if(pagina.equals("buscar")){	
+			request.setAttribute("tituloPagina", "Resultados de tu busqueda:");
 			juegos = ListadoDao.recuperarListaPorBusqueda(request.getParameter("busqueda"));
 		}
 		
 		
 		sesion.setAttribute("listadoJuegos", juegos);
 		
-		request.setAttribute("tituloPagina", "Todos nuestros productos");
+		
 		
 		RequestDispatcher view = request.getRequestDispatcher("listado.jsp");
 		view.forward(request, response);
